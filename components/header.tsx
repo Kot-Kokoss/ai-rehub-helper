6fdd3996615af83bac3dfb2b03cebf241d512f83
Kot-Kokoss <izaostrovskiy@mail.ru>
2025-09-23 00:00:18 +0500

    initial commit

---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import { ModeToggle } from './ui/modeToggle';
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-between border-b-2 p-3">
+      <h1 className="font-mono text-3xl">Ai rehub helper</h1>
+      <ModeToggle></ModeToggle>
+    </header>
+  );
+}
